5252ffae11e8bc1dd0522c3d6e33819fb4646e21
dthiers <dthiers@student.avans.nl>
2022-02-22 16:42:51 +0100

    Class Diagram

--- CODE_Bagageband/obj/Debug/App.g.i.cs
+++ CODE_Bagageband/obj/Debug/App.g.i.cs
@@ -59,7 +59,7 @@ namespace CODE_Bagageband {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/CODE_Bagageband;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DPINT_Wk3_Observer;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- CODE_Bagageband/obj/Debug/MainWindow.g.i.cs
+++ CODE_Bagageband/obj/Debug/MainWindow.g.i.cs
@@ -52,7 +52,7 @@ namespace CODE_Bagageband {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CODE_Bagageband;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DPINT_Wk3_Observer;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
